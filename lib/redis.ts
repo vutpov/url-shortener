e57fd1029e09fd0b782d77daffefba79c4a70d37
vutpov <vuthpov26@gmail.com>
2026-04-26 18:57:33 +0700

    use upstash env

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,8 +1,5 @@
 import "server-only";
 import { Redis } from "@upstash/redis";
-const redis = new Redis({
-  url: process.env.REDIS_URL,
-  token: process.env.REDIS_TOKEN,
-});
+const redis = Redis.fromEnv();
 
 export default redis;
