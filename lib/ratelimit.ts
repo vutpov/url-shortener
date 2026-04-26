b7ee06b7bc907674f9902552bff64f9d4a7cd0d6
vutpov <vuthpov26@gmail.com>
2026-04-26 18:29:49 +0700

    update

---
+++ lib/ratelimit.ts
@@ -0,0 +1,7 @@
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+export const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(10, "10 s"), // 10 requests per 10 seconds
+});
